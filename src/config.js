569b182f98ceb447b65584e997483bf743ce8eef
Jie Su <jsumz@amazon.com>
2020-04-20 01:45:50 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-uploads-jie"
+  },
+  apiGateway: {
+    REGION: "ues-west-2",
+    URL: "prod-notes-app-api"
+  },
+  cognito: {
+    REGION: "us-west-2",
+    USER_POOL_ID: "us-west-2_rwmie9mOS",
+    APP_CLIENT_ID: "4d74vl0oulhnskud72u73at9ok",
+    IDENTITY_POOL_ID: "us-west-2:5a6afffb-649d-4ea3-83f1-76e0b96ce7aa"
+  }
+};
